d5713ef9391f35c8506f2ae40d4357959023fdda
candywater <candywater@users.noreply.github.com>
2017-07-29 22:51:44 +0900

    fix image size

--- project/9yin/tools/update_from_git_dir.sh
+++
@@ -1,4 +0,0 @@
-# for local pc
-cp ../*.html ~/work_space/git_dirs/tools/game_tools/ageOfWushu/9yin/
-cp -r ../lib/ ~/work_space/git_dirs/tools/game_tools/ageOfWushu/9yin/
-cp -r ../assets/ ~/work_space/git_dirs/tools/game_tools/ageOfWushu/9yin/
