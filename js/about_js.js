e6a36d75976a2287ab588afc79b8c800ddf71387
candywater <candywater@users.noreply.github.com>
2019-04-21 11:14:58 +0800

    refactoring

---
+++ js/about_js.js
@@ -0,0 +1,29 @@
+import "./copyleft.js"
+var meow_flag = false;
+var credit_info_flag = false;
+jQuery(function(){
+  jQuery("#meow").hide();
+  jQuery("#credit_info").hide();
+
+  jQuery("#profile_photo").click(function(){
+    meow_flag = !meow_flag;
+    if(meow_flag)
+      jQuery("#meow").show();
+    else {
+      jQuery("#meow").hide();
+    }
+    //jQuery("#meow").animate({left:"250px"})
+  })
+
+  jQuery("#credit").click(function(){
+    credit_info_flag = !credit_info_flag;
+    if(credit_info_flag)
+      jQuery("#credit_info").show();
+    else {
+      jQuery("#credit_info").hide();
+    }
+
+  })
+
+
+})
