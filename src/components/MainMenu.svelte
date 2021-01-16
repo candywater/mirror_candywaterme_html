150a959ca4d47c30f5895a82e1a581df28428ff8
candywater <candywater@github.com>
2021-01-16 12:12:43 +0800

    feat: 🎸 changed main menu

--- src/components/MainMenu.svelte
+++ src/components/MainMenu.svelte
@@ -7,35 +7,37 @@
   const about = "/about"
   const index = "/"
 
+  const button_style_1 = "bg-blue-500 hover:bg-blue-700 text-white rounded"
+  const button_style_2 = "bg-indigo-500 hover:bg-indigo-700 text-white rounded"
+  const button_style_3 = "bg-purple-500 hover:bg-purple-700 text-white rounded"
+  const button_style_4 = "bg-yellow-500 hover:bg-yellow-700 text-white rounded"
+  const button_style_5 = "bg-red-500 hover:bg-red-700 text-white rounded"
+
 </script>
 
 <div class="main-menu">
 
-  <a href="/blog/essay" role="button" class="btn btn-light btn-lg " name="button" id="blog_btn" >Essay</a>
-  <a href="/blog/tech" role="button" class="btn btn-primary btn-lg " name="button" id="blog_btn" >Tech</a>
-  <a href="/project" role="button" class="btn btn-success btn-lg " name="button" id="project_btn" >Project</a>
-  <a href="/about" role="button" class="btn btn-danger btn-lg " name="button" id="about_btn" >About</a>
-  <a href="/" role="button" class="btn btn-warning btn-lg " name="button" id="main_btn" >Main</a>
-
+  <a href="/blog/essay" role="button" name="button"
+    class="btn {button_style_1} ">Essay</a>
+  <a href="/blog/tech" role="button" name="button"
+    class="btn {button_style_2}">Tech</a>
+  <a href="/project" role="button" name="button"
+    class="btn {button_style_3}">Project</a>
+  <a href="/about" role="button" name="button"
+    class="btn {button_style_4}">About</a>
+  <a href="/" role="button" name="button"
+    class="btn {button_style_5}">Main</a>
 </div>
 
-<style lang="scss">
-
-//from bootstrap
-@import "bootstrap/scss/mixins/_gradients.scss";
-@import "bootstrap/scss/mixins/_box-shadow.scss";
-@import "bootstrap/scss/mixins/_transition.scss";
-@import "bootstrap/scss/mixins/_border-radius.scss";
-@import "bootstrap/scss/mixins/_buttons.scss";
-@import "bootstrap/scss/vendor/_rfs.scss";
-@import "bootstrap/scss/_functions.scss";
-@import "bootstrap/scss/_variables.scss";
-@import "bootstrap/scss/_buttons.scss";
-
+<style>
 .btn{
+  display: block;
   width: 7rem;
-  margin: 0.1rem 0rem;
-  padding: 0.4rem;
+  margin: 0.2rem 0rem;
+  padding: 0.5rem 0.7rem;
+  font-size: 1.25rem;
+  text-align: center;
+  font-weight: 400;
 }
 .main-menu{
   width: 7rem;
