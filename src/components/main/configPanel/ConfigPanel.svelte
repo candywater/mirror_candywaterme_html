889d23a13ca87f39f8c58f12fafb3773d53dba8d
candywater <candywater@github.com>
2021-01-17 11:12:37 +0800

    refactor: 💡 delete test code

--- src/components/main/configPanel/ConfigPanel.svelte
+++ src/components/main/configPanel/ConfigPanel.svelte
@@ -1,28 +1,16 @@
 <script>
 
 import {panel_background_color} from "../../../store/config"
-import {hex2RGB} from "../../../common/common"
+// import {hex2RGB} from "../../../common/common"
 
 const btn_style = "btn bg-blue-500 hover:bg-blue-700"
-
-function OnBtnClick(){
-  console.log($panel_background_color)
-}
-
-let color_picker = ""
-
-function OnChanged(){
-  $panel_background_color = color_picker
-  // $panel_background_color = `rgba( ${hex2RGB(color_picker).join(',')}, 0.3);`
-  console.log($panel_background_color)
-}
 
 </script>
 
 <div class="panel">
   <div>
     <label>Panel Color Picker</label>
-    <input type="color" bind:value={color_picker} on:change={OnChanged}>
+    <input type="color" bind:value={$panel_background_color} />
   </div>
 </div>
 
@@ -30,11 +18,13 @@ function OnChanged(){
   $white-background : rgba(156, 163, 175, 0.7);
   .panel{
     background-color: $white-background;
+    padding: 1rem;
     position: absolute;
     width: 100%;
     height: 100%;
     display: grid;
-    padding: 1rem;
+    justify-content:center;
+    align-items: flex-start;
     div{
     }
   }
