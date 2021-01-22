70bb467352ea10cd961a6657894fc83b77b4ecdc
candywater <candywater@github.com>
2021-01-22 19:45:46 +0800

    refactor: 💡 修正效果

--- src/_layout.svelte
+++ src/_layout.svelte
@@ -66,22 +66,13 @@ function OnAnimationEnd(){
 
 <style global lang="scss">
 //https://www.geeksforgeeks.org/how-to-set-fullscreen-iframe/
-html{
-  overflow: auto;
-}
-html, body, div, iframe 
+// make html has scroll bar
+html
 {
- margin: 0px; 
+ /* margin: 0px;  */
  padding: 0px; 
- height: 100%; 
- border: none;
-}
-iframe {
-  display: block; 
-  width: 100%; 
-  border: none; 
-  overflow-y: auto; 
-  overflow-x: hidden;
+ /* height: 100%; 
+ border: none; */
 }
 
 body{
