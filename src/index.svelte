3450d5ebdf5952f39d2075b9f1abce32fe83217c
candywater <candywater@users.noreply.github.com>
2021-02-23 23:34:29 +0800

    refactor: 💡 delete useless code

--- src/index.svelte
+++ src/index.svelte
@@ -1,12 +1,6 @@
 <script>
 import Main from "./_main_layout.svelte";
 
-const quote_url = ""
-const quote_list = ""
-const description = ""
-
 </script>
 
-<Main {quote_url} {quote_list} {description}>
-
-</Main>
+<Main> </Main>
