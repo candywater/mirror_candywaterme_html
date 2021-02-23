531ae010cfd911d5e43f6fdbc7f1161ef0afac07
candywater <candywater@users.noreply.github.com>
2021-02-23 23:30:38 +0800

    docs: ✏️ update project

--- src/components/project/ProjectList.svelte
+++ src/components/project/ProjectList.svelte
@@ -1,11 +1,7 @@
 
 
 <script>
-  let list = [
-     ["https://candywater.xyz/android-download", "九阴计算器apk"],
-     ["https://candywater.xyz", "九阴计算器"],
-     ["#", "discord chat bot"],
-  ]
+  import list from "../../data/project_list"
 
   let list_type = [
     "bg-gray-300 hover:bg-gray-500 text-black rounded"
@@ -17,7 +13,7 @@
 <ol class="list-disc">
   {#each list as item, i}
     <li>
-      <a href={item[0]} class="{list_type[i%list_type.length]}">{item[1]}</a>
+      <a href={item[0]} class="{list_type[i%list_type.length]}">{item[1]} ({item[2]})</a>
     </li>
   {/each}
 </ol>
---
+++ src/data/project_list.js
@@ -0,0 +1,7 @@
+
+export default [
+  ["https://candywater.xyz/android-download", "九阴计算器apk", "2017"],
+  ["https://candywater.xyz", "九阴计算器", "2017"],
+  ["#", "discord chat bot", "2020"],
+  ["https://github.com/candywater/svelte-terminal", "svelte-terminal", "2020"],
+]
