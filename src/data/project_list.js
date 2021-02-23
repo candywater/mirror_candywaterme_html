96a0757dc86171753e309413e8670e0203e21895
candywater <candywater@users.noreply.github.com>
2021-02-24 00:27:44 +0800

    docs: ✏️ update project list

--- src/data/project_list.js
+++ src/data/project_list.js
@@ -2,6 +2,7 @@
 export default [
   ["https://candywater.xyz/android-download", "九阴计算器apk", "2017"],
   ["https://candywater.xyz", "九阴计算器", "2017"],
-  ["#", "discord chat bot", "2020"],
-  ["https://github.com/candywater/svelte-terminal", "svelte-terminal", "2020"],
+  ["#", "wechat html app/frontend/backend(private)", "2019"],
+  ["#", "discord chat bot(private)", "2020"],
+  ["https://github.com/candywater/svelte-terminal", "svelte-terminal", "2021"],
 ]
