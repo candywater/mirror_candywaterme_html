fe6b08900c7b248dc9427f039f374aac94fdcf5c
candywater <candywater@users.noreply.github.com>
2022-01-01 23:54:32 +0800

    remove is_hide_all_content

--- src/store/config.ts
+++ src/store/config.ts
@@ -1,7 +1,6 @@
 import { writable } from "svelte/store";
 
 export const panel_background_color = writable("rgba(245, 245, 245, 0.3);");
-export const is_hide_all_content = writable(false);
 export const show_config_panel = writable(false);
 export const show_quote = writable(false);
 
