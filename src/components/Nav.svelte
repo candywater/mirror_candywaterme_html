5541598fd5463681db2abd3c540b8e89350b27ca
candywater <candywater@users.noreply.github.com>
2022-01-15 00:45:09 +0800

    refactor

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
-  import { path } from "../config/path";
-import MainMenu from "./main/MainMenu.svelte";
+  import MainMenu from "./main/MainMenu.svelte";
 </script>
 
-<MainMenu></MainMenu>
+<MainMenu />
