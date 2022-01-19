d8680ed8ffcacabcebb2466208e9a1df3d02f657
candywater <candywater@users.noreply.github.com>
2022-01-19 22:50:19 +0800

    fix icon

--- src/components/post/headerlist.svelte
+++ src/components/post/headerlist.svelte
@@ -6,12 +6,22 @@
     function escapedText(text: string) {
         return text.toLowerCase().replace(/[^\w]+/g, "-");
     }
+    //Ø \00d8
+    //θ \03b8
+    //φ \03C6
+    //† \2020
+    //‡ \2021
+    //✎ \270E
+    //✝ \271D
+    //✡ \2721
+    //✭ \272D
+    //✩ \2729
 </script>
 
 <div class="headernav mt-1">
     {#await headerList then headerList}
             {#each headerList as headerPair}
-                <a class="before:content-['\00d8'] mr-2 border pl-1 pr-1" href={"#" + escapedText(headerPair.text)}>{headerPair.text}</a>
+                <a class="before:content-['\270E'] mr-2 border pl-1 pr-1" href={"#" + escapedText(headerPair.text)}>{headerPair.text}</a>
             {/each}
     {/await}
 </div>
