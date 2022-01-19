6541fb06111dfa1b70559dbabea91b8f2ebae376
candywater <candywater@users.noreply.github.com>
2022-01-19 22:03:42 +0800

    fix heading again

--- src/components/markdown/markdownParse.ts
+++ src/components/markdown/markdownParse.ts
@@ -4,6 +4,23 @@ marked.setOptions({
   breaks: true,
 });
 
+const renderer = {
+  heading(text, level) {
+    const escapedText = text.toLowerCase().replace(/[^\w]+/g, '-');
+    console.log(text, level)
+
+    return `
+            <h${level}>
+              <a name="${escapedText}" class="anchor" href="#${escapedText}">
+                <span class="header-link"></span>
+              </a>
+              <span>${text}</span>              
+            </h${level}>`;
+  },
+};
+
+marked.use({renderer})
+
 export function markdownParse(content) {
   return marked.parse(content);
 }
