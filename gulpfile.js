67ec719caf1f473685335028f4ca1e2775c58592
candywater <candywater@users.noreply.github.com>
2022-01-23 23:54:27 +0800

    rename minima -> main

--- gulpfile.js
+++ gulpfile.js
@@ -105,8 +105,8 @@ function main_watch(){
 // exports["css:dev"] = gulp.series(purgecss_tailwindcss_dev, complier_sass_dev);
 // exports["sass"] = gulp.series(complier_sass_dev)
 // exports["sass:watch"] = gulp.series(complier_sass_watch)
-exports.default = gulp.series( minima_prod, complier_main_prod, tailwindcss_prod);
-exports.dev = gulp.series( minima_dev, main_dev, tailwind_dev);
-exports["watch"] = gulp.parallel( minima_watch, main_watch, tailwind_watch)
+exports.default = gulp.series(  complier_main_prod, tailwindcss_prod);
+exports.dev = gulp.series(  main_dev, tailwind_dev);
+exports["watch"] = gulp.parallel(  main_watch, tailwind_watch)
 
 // postcss
