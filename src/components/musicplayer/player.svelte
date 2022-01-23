2edb3f8f0f717ba5dd6434087db4f8a72fbbe849
candywater <candywater@users.noreply.github.com>
2022-01-23 16:52:47 +0800

    for player

---
+++ src/components/musicplayer/player.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    import { Howl, Howler } from "howler";
+    import { onMount } from "svelte";
+
+    export let bgm_list: string[];
+    let _bgm_list: string[];
+    let _sound : Howl;
+
+    const music_prefix = "/doc/music/";
+
+    onMount(() => {
+        bgm_list.forEach((element) => {
+            _bgm_list.push(music_prefix + element);
+        });
+        _sound = new Howl({
+            src: _bgm_list,
+            html5: true,
+            volume: 0.5,
+            onend: function () {
+                console.log("Finished!");
+            },
+        });
+    });
+
+    function onplay() {
+        _sound.play();
+    }
+    
+</script>
