f7d782bd5ac8682e3c20fb59a918d7889fb6ccea
candywater <candywater@users.noreply.github.com>
2022-06-12 14:55:03 +0800

    move back ground back to main.scss

--- src/template/_layout.svelte
+++ src/template/_layout.svelte
@@ -17,41 +17,11 @@
 </main>
 
 <style global lang="scss">
-  body {
-    /*idea: https://codepen.io/P1N2O/pen/pyBNzX*/
-    background: linear-gradient(
-        -45deg,
-        rgb(179, 108, 87),
-        rgba(155, 44, 44, 0.637),
-        rgb(19, 90, 116),
-        rgba(29, 104, 86, 0.534)
-      ),
-      linear-gradient(
-        127deg,
-        rgb(87, 44, 31),
-        rgb(11, 75, 99),
-        rgba(17, 100, 81, 0.534)
-      );
-    background-size: 400% 400%;
-    animation: Gradient 25s ease infinite;
-  }
   .hide {
     display: none !important;
   }
   .show {
     display: block !important;
-  }
-
-  @keyframes Gradient {
-    0% {
-      background-position: 0% 50%;
-    }
-    50% {
-      background-position: 75% 50%;
-    }
-    100% {
-      background-position: 0% 50%;
-    }
   }
 
   main {
