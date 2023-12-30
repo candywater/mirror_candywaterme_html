af1450c78b5ccde742c3d0d328c990f6740837ba
candywater <candywater@users.noreply.github.com>
2023-12-30 21:32:27 +0800

    add blocking

--- src/html/components/encryption/aes.svelte
+++ src/html/components/encryption/aes.svelte
@@ -8,6 +8,7 @@
         src="https://cdn.jsdelivr.net/npm/aes-js@3.1.2/index.min.js"
         integrity="sha512-qe3tMl5So3PPvTaPsfRHXrW7G3i9QK+Q34WzyIrZAtt1ph2Wwd8od+yOyRLbUeVG24rDaWAPpUmnIrxON8aPGw=="
         crossorigin="anonymous"
+        blocking="render"
         on:load={initializeAes}
     ></script>
 </svelte:head>
