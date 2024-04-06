562cee01de4df489751415f5e7c2b95cf69d30fd
candywater <candywater@users.noreply.github.com>
2024-04-06 12:47:04 +0800

    fix aes import

--- src/pages/index/html/components/encryption/aes.svelte
+++ src/pages/index/html/components/encryption/aes.svelte
@@ -4,11 +4,16 @@
 </script>
 
 <svelte:head>
-    <script
+    <script src="https://cdn.jsdelivr.net/npm/aes-js@3.1.2/index.min.js" integrity="sha512-IgHvpZTy/dirq0pAibTyQkWRONhi5tc3vSW6QqfuDPJsnkPrmyCJP94BcDgcEQZ8sMLX9k5J1Gn67RH5uTPhNA==" 
+    crossorigin="anonymous"    
+    blocking="render"
+    on:load={initializeAes}>
+    </script>
+    <!-- <script
         src="https://cdn.jsdelivr.net/npm/aes-js@3.1.2/index.min.js"
         integrity="sha512-qe3tMl5So3PPvTaPsfRHXrW7G3i9QK+Q34WzyIrZAtt1ph2Wwd8od+yOyRLbUeVG24rDaWAPpUmnIrxON8aPGw=="
         crossorigin="anonymous"
         blocking="render"
         on:load={initializeAes}
-    ></script>
+    ></script> -->
 </svelte:head>
