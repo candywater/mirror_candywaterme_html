95dcbb21cad126260cb5c199ff2d5235fc57869d
candywater <candywater@users.noreply.github.com>
2024-10-20 20:32:40 +0800

    add navigate comment

--- src/ts/config/path.ts
+++ src/ts/config/path.ts
@@ -1,7 +1,7 @@
 import { writable } from "svelte/store";
 
 export const INDEX = "main";
-export const TIMES = "times";
+export const TIMES = "times"; // navigated by nginx and folder structure
 export const ABOUT = "about";
 export const PROJECT = "project";
 export const ESSAY = "essay";
@@ -15,7 +15,7 @@ export const GITHUB_OAUTH_CALLBACK = "github-oauth-callback";
 const INDEX_PATH = "/";
 const ABOUT_PATH = "/about";
 const PROJECT_PATH = "/project";
-const TIMES_PATH = "/pages/times";
+const TIMES_PATH = "/pages/times"; // navigated by nginx and folder structure
 const ESSAY_PATH = "/blog/essay";
 const TECH_PATH = "/blog/tech";
 const YEAR_SUMMARY_PATH = "/blog/year-summary";
@@ -40,7 +40,7 @@ export const GITHUB_OAUTH_CALLBACK_URL = "/github/oauth_callback";
 
 export let PATH_DICT: Record<string, string> = {};
 PATH_DICT[INDEX] = INDEX_PATH;
-PATH_DICT[TIMES] = TIMES_PATH;
+PATH_DICT[TIMES] = TIMES_PATH; // navigated by nginx and folder structure
 PATH_DICT[ABOUT] = ABOUT_PATH;
 PATH_DICT[PROJECT] = PROJECT_PATH;
 PATH_DICT[ESSAY] = ESSAY_PATH;
